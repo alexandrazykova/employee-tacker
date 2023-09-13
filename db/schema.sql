e141567da7b87434e46054098161c20deadd60b4
alexandrazykova <alexandrazykova1@gmail.com>
2023-09-13 15:59:30 -0400

    created table employee

--- db/schema.sql
+++ db/schema.sql
@@ -19,3 +19,13 @@ CREATE TABLE role (
   REFERENCES department(id)
   ON DELETE CASCADE
 );
+
+CREATE TABLE employee (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  slast_name VARCHAR(30),
+  role_id INT
+ manager_id INT 
+ FOREIGN KEY()
+--  manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+);
