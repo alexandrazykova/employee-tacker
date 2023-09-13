e1655427ba03589dad8cea04708f9f7f1d513f67
alexandrazykova <alexandrazykova1@gmail.com>
2023-09-13 16:11:31 -0400

    added insert into employee table

--- db/schema.sql
+++ db/schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE role (
 CREATE TABLE employee (
   id INT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
-  slast_name VARCHAR(30),
+  last_name VARCHAR(30),
   role_id INT
  manager_id INT 
  FOREIGN KEY()
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,12 +5,24 @@ VALUES (1, 'Sales'),
        (4, "Legal");
 
 
-INSERT INTO role (id, title, department, salary)
-VALUES (1, 'Sales Lead', 'Sales', 100000),
-       (2, "Salesperson", "Sales", 80000),
-       (3, 'Lead Engineer', 'Engineering', 150000),
-       (4, "Software Engineer", 'Engineering', 120000),
-       (5, "Account Manager", 'Engineering', 160000),
-       (6, 'Accountant', 'Finance', 125000),
-       (8, "Legal Team Lead", 'Finance', 250000),
-       (8, 'Lawyer', 'Finance', 190000);
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, 'Sales Lead', 100000, 'Sales', 1),
+       (2, "Salesperson", 80000, "Sales", 1),
+       (3, 'Lead Engineer', 150000, 'Engineering', 2),
+       (4, "Software Engineer", 120000, 'Engineering', 2),
+       (5, "Account Manager", 160000, 'Engineering', 2),
+       (6, 'Accountant', 125000, 'Finance', 3),
+       (7, "Legal Team Lead", 250000, 'Finance', 3),
+       (8, 'Lawyer', 190000, 'Finance', 3);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES
+    (1,'John', 'Doe', 1, 1),
+    (2, 'Mike', 'Chan', 2, 2),
+    (3, 'Ashley', 'Rodrigues', 3, 3),
+    (4, 'Kevin', 'Tupik', 4, 4),
+    (5, 'Kunal', 'Singh', 5, 5),
+    (6, 'Malia', 'Brown', 6, 6),
+    (7, 'Sarah', 'Lourd', 7, 7),
+    (8, 'Tom', 'Allen', 8, 8);
+
