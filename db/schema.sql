120bc29d9e3b323ad8b02b1d429aad95ef2dc842
alexandrazykova <alexandrazykova1@gmail.com>
2023-09-13 21:31:26 -0400

    added auto_increment to id(s) and fixed addDepartment bug accordingly

--- db/schema.sql
+++ db/schema.sql
@@ -6,12 +6,12 @@ USE employee_db;
 
 
 CREATE TABLE department (
-  id INT NOT NULL PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-  id INT PRIMARY KEY NOT NULL,
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL,
   department_id INT,
@@ -21,7 +21,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-  id INT PRIMARY KEY,
+  id INT PRIMARY AUTO_INCREMENT KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30),
   role_id INT,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,28 +1,28 @@
-INSERT INTO department (id, name)
-VALUES (1, 'Sales'),
-       (2, "Engineering"),
-       (3, 'Finance'),
-       (4, "Legal");
+INSERT INTO department (name)
+VALUES ('Sales'),
+       ("Engineering"),
+       ('Finance'),
+       ("Legal");
 
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (1, 'Sales Lead', 100000, 1),
-       (2, "Salesperson", 80000, 1),
-       (3, 'Lead Engineer', 150000, 2),
-       (4, "Software Engineer", 120000, 2),
-       (5, "Account Manager", 160000, 2),
-       (6, 'Accountant', 125000, 3),
-       (7, "Legal Team Lead", 250000, 3),
-       (8, 'Lawyer', 190000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Sales Lead', 100000, 1),
+       ("Salesperson", 80000, 1),
+       ('Lead Engineer', 150000, 2),
+       ("Software Engineer", 120000, 2),
+       ("Account Manager", 160000, 2),
+       ('Accountant', 125000, 3),
+       ("Legal Team Lead", 250000, 3),
+       ('Lawyer', 190000, 3);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-    (1,'John', 'Doe', 1, 1),
-    (2, 'Mike', 'Chan', 2, 2),
-    (3, 'Ashley', 'Rodrigues', 3, 3),
-    (4, 'Kevin', 'Tupik', 4, 4),
-    (5, 'Kunal', 'Singh', 5, 5),
-    (6, 'Malia', 'Brown', 6, 6),
-    (7, 'Sarah', 'Lourd', 7, 7),
-    (8, 'Tom', 'Allen', 8, 8);
+    ('John', 'Doe', 1, 1),
+    ('Mike', 'Chan', 2, 2),
+    ('Ashley', 'Rodrigues', 3, 3),
+    ('Kevin', 'Tupik', 4, 4),
+    ('Kunal', 'Singh', 5, 5),
+    ('Malia', 'Brown', 6, 6),
+    ('Sarah', 'Lourd', 7, 7),
+    ('Tom', 'Allen', 8, 8);
 
